78a4b4154799d8e7d255f6a642e5dc9d2e20cbae
Cody Fayolle <cfayolle@umich.edu>
2018-05-03 11:40:20 -0500

    going to pull

--- native/components/select-tasks.js
+++ native/components/select-tasks.js
@@ -1,28 +1,87 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Container, Header, Content, List, ListItem, Button} from 'native-base'
+import {Container, Header, Content, List, ListItem, Button, Right, Card, CardItem, Body} from 'native-base'
+
+
+const dummyTasks = [
+  {task: 'Clean bathroom', daysSinceCompleted: 6, pts: 7},
+  {task: 'Take out trash', daysSinceCompleted: 1, pts: 0},
+  {task: 'Vacuum living room', daysSinceCompleted: 10, pts: 10},
+  {task: 'Sweep kitchen', daysSinceCompleted: 2, pts: 1},
+  {task: 'Random task', daysSinceCompleted: 16, pts: 17},
+  {task: 'Random task', daysSinceCompleted: 11, pts: 20},
+  {task: 'Random task', daysSinceCompleted: 13, pts: 12},
+  {task: 'Random task', daysSinceCompleted: 7, pts: 19},
+]
+
 
 export default class SelectTasks extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      tasks: {},
+    }
+  }
+
   render() {
+    const ok = this.state.tasks
+    const sortedTasks = mergeSortTasks(dummyTasks)
     return (
       <Container style={styles.list}>
           <Content>
-            <List>
-              <ListItem><Text>Clean Living Room</Text></ListItem>
-              <ListItem><Text>Sweep the Kitchen</Text></ListItem>
-              <ListItem><Text>Take out the Trash</Text></ListItem>
-              <ListItem><Text>Clean the Bathroom</Text></ListItem>
-            </List>
-          <Button onPress={() => this.props.navigation.navigate('Select')} style={styles.button}><Text>Click Me</Text></Button>
-          <Button onPress={() => this.props.navigation.navigate('Home')}><Text>Go Back</Text></Button>
+          {sortedTasks.map(task => {
+            return (
+              <Card key={task.daysSinceCompleted}>
+                <CardItem header style={styles.header}>
+                  <Text>{task.task}</Text>
+                  <Text style={styles.score}>{task.pts}</Text>
+                </CardItem>
+                <CardItem>
+                  <Text>Last completed {task.daysSinceCompleted} days ago</Text>
+                </CardItem>
+              </Card>
+            )
+          })}
           </Content>
       </Container>
     );
   }
 }
 
+const mergeSortTasks = (tasks) => {
+  if (tasks.length === 1) {
+    return tasks
+  }
+  const middle = Math.floor(tasks.length / 2)
+  const left = tasks.slice(0, middle)
+  const right = tasks.slice(middle)
+  return merge(
+    mergeSortTasks(left),
+    mergeSortTasks(right),
+  )
+}
+
+const merge = (left, right) => {
+  let result = []
+  let indexLeft = 0
+  let indexRight = 0
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft].pts > right[indexRight].pts) {
+      result.push(left[indexLeft])
+      indexLeft++
+    } else {
+      result.push(right[indexRight])
+      indexRight++
+    }
+  }
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+}
 const styles = StyleSheet.create({
   list: {
     backgroundColor: '#fff',
   },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
 });
