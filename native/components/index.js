226149510b923424eb413c1f86a0bb19d8bdc271
chrisstpehenmiller <csmiller4567@gmail.com>
2018-05-03 11:09:30 -0500

    scores

--- native/components/index.js
+++ native/components/index.js
@@ -1,2 +1,3 @@
 export {default as HomeScreen} from './home'
 export {default as SelectTasks} from './select-tasks'
+export {default as Scores} from './scores'
--- native/root-stack.js
+++ native/root-stack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import {HomeScreen, SelectTasks} from './components'
+import { HomeScreen, SelectTasks, Scores } from './components'
 
 const RootStack = StackNavigator({
   Home: {
@@ -9,6 +9,9 @@ const RootStack = StackNavigator({
   },
   Select: {
     screen: SelectTasks
+  },
+  Scores: {
+    screen: Scores
   }
 })
 
