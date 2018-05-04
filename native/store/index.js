ac320bb0619badb50e10a5a31ce920cdb26cd3d7
Cody Fayolle <cfayolle@umich.edu>
2018-05-04 10:17:58 -0500

    small changes

--- native/store/index.js
+++ native/store/index.js
@@ -2,9 +2,10 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import User from './auth'
 //import user from './auth'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({User})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
