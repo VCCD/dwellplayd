5e1eb9d45fad74e2f61605c83eeb3702629642cd
Vi Tran <vipran@gmail.com>
2018-05-03 19:58:15 -0500

    login working?

--- native/App.js
+++ native/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Text } from 'native-base';
-import RootStack from './root-stack'
+import RootStack from './root-stack';
+import {Provider} from 'react-redux'
+import store from './store'
+
 
 
 export default class App extends React.Component {
   render() {
     return (
+      
+      <Provider store={store}>
       <RootStack />
+      </Provider>
+  
+
     );
   }
 }
-
 
--- native/components/login.js
+++ native/components/login.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {connect} from 'react-redux'
+import {auth} from '../store/auth'
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Container, Header, Content, Form, Item, Input, Label, Button, Icon } from 'native-base';
 
 
-export default class LoginScreen extends Component {
-  render() {
+
+
+    const LoginScreen = (props) => {
+      const state = {}
+      const {name, handleSubmit} = props
     return (
 
      
 
       <Container style={styles.container}>
       
-        <Header />
+
         <Content>
-          <Form>
+          <Form name={name}>
             <Item inlineLabel>
-              <Label style={styles.titleText}>Username</Label>
-              <Input />
+              <Label style={styles.titleText} >Email</Label>
+              <Input name="email" onChangeText={text => state.email = text } />
             </Item>
             <Item inlineLabel last>
-              <Label style={styles.titleText}>Password</Label>
-              <Input />
+              <Label style={styles.titleText} name="password">Password</Label>
+              <Input onChangeText={(text) => state.password = text} />
             </Item>
-
+            <Button block onPress={loginSubmit}><Text>Login</Text></Button>
           </Form>
-          <Button block><Text>Login</Text></Button>
-            <Button block>
+          
+          <Button block onPress={(event)=> console.log(event.target.value)}><Text>Create Account</Text></Button>
+            <Button block onPress={()=> console.log(state)}>
               <Icon />
               <Text>Sign in with Google</Text>
             </Button>
@@ -35,7 +41,7 @@ export default class LoginScreen extends Component {
 
     );
   }
-}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -51,3 +57,28 @@ const styles = StyleSheet.create({
 
   },
 });
+
+//mapLogin = null
+//mapDispatch = null
+
+const mapLogin = (state) => {
+  return {
+    name: 'login',
+    displayName: 'Login',
+    //error: state.user.error
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    loginSubmit (evt) {
+      console.log(evt.target)
+      // evt.preventDefault()
+     
+      // const email = evt.target.email.value
+      // const password = evt.target.password.value
+       dispatch(auth(this.state))
+    }
+  }
+}
+export default connect(mapLogin, mapDispatch)(LoginScreen)
