594347344b5b214c53ac4fedddebc294eb54cece
David Fudacz <davidfudacz@gmail.com>
2018-05-07 15:44:56 -0500

    emails sending properly

--- server/api/communities.js
+++ server/api/communities.js
@@ -1,6 +1,19 @@
 const router = require('express').Router()
 const { Community, User } = require('../db/models')
+const sendEmail = require('../mailer')
 module.exports = router
+
+//mailer takes --> email, user, community, status
+
+router.param('communityId', async (req, res, next, communityId) => {
+  try {
+    req.community = await Community.findById(communityId)
+    next()
+  }
+  catch (err) {
+    next(err)
+  }
+})
 
 router.get('/:communityId', (req, res, next) => {
   const id = req.params.communityId
@@ -9,4 +22,17 @@ router.get('/:communityId', (req, res, next) => {
   })
     .then(community => res.json(community))
     .catch(next)
+})
+
+router.post('/:communityId/inviteUsers', (req, res, next) => {
+  try {
+    const user = req.body.user
+    const emails = req.body.emails
+    emails.forEach(email => {
+      sendEmail(email, user, req.community, 'invite')
+    })
+  }
+  catch (err) {
+    next(err)
+  }
 })
