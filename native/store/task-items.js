1c6819e741e7f082aa12bf59ea4ccb6a95ce1504
Vi Tran <vipran@gmail.com>
2018-05-07 12:10:20 -0500

    render user community task data

--- native/store/task-items.js
+++ native/store/task-items.js
@@ -3,35 +3,23 @@ import CONFIG from '../api-routes'
 
 const apiURL = CONFIG.API_URL
 
-const EDIT_TASK_ITEM = 'EDIT_TASK_ITEM'
-const GET_ALL_COMMUNITY_TASKS = 'GET_ALL_COMMUNITY_TASKS'
-const CLEAR_COMMUNITY_TASKS = 'CLEAR_COMMUNITY_TASKS'
+const SELECT_TASK_COMPLETE = 'SELECT_TASK_COMPLETE'
+const GET_ALL_INCOMEPLETE_COMMUNITY_TASKS = 'GET_ALL_INCOMEPLETE_COMMUNITY_TASKS'
 
 //action creators
 
-export const addCommunityTask = (task) => ({
-  type: ADD_COMMUNITY_TASK,
-  task,
-})
 
-export const removeCommunityTask = (task) => ({
-  type: REMOVE_COMMUNITY_TASK,
-  task,
-})
-
-export const editCommunityTask = (task) => ({
+export const selectTaskComplete = (task) => ({
   type: EDIT_TASK_ITEM,
   task,
 })
 
-export const getAllCommunityTasks = (tasks) => ({
-  type: GET_ALL_COMMUNITY_TASKS,
+export const getAllIncompleteCommunityTasks = (tasks) => ({
+  type: GET_ALL_INCOMEPLETE_COMMUNITY_TASKS,
   tasks
 })
 
-export const clearCommunityTasks = () => ({
-  type: CLEAR_COMMUNITY_TASKS,
-})
+
 
 //thunks
 
@@ -39,7 +27,7 @@ export const getAllTaskItemsFromServerThunkerator = (communityId) => {
   return async (dispatch) => {
     try {
       const tasks = await axios.get(`${apiURL}/community-tasks/${communityId}`)
-      dispatch(getAllCommunityTasks(tasks.data))
+      dispatch(getAllIncompleteCommunityTasks(tasks.data))
     }
     catch (err) {
       console.log(err)
@@ -50,19 +38,14 @@ export const getAllTaskItemsFromServerThunkerator = (communityId) => {
 //reducer
 export default (prevState = [], action) => {
   switch (action.type) {
-    case ADD_COMMUNITY_TASK:
-      return [...prevState, action.task]
-    case REMOVE_COMMUNITY_TASK:
-    return prevState.filter(task => task.id !== action.task.id)
-    case EDIT_TASK_ITEM:
+    case SELECT_TASK_COMPLETE:
         return prevState.map(task => {
           if (task.id === action.task.id) return action.task
           else return task
         })
-    case GET_ALL_COMMUNITY_TASKS:
+    case GET_ALL_INCOMEPLETE_COMMUNITY_TASKS:
       return action.tasks
-    case CLEAR_COMMUNITY_TASKS:
-      return []
+   
     default: return prevState
   }
 }
