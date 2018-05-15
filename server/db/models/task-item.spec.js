77205927b1b99bfebf3f55dbe31745ed1d8084a8
chrisstpehenmiller <csmiller4567@gmail.com>
2018-05-15 12:50:53 -0500

    taskItem model tests

--- server/db/models/task-item.spec.js
+++ server/db/models/task-item.spec.js
@@ -5,28 +5,44 @@ const db = require('../index')
 const TaskItem = db.model('taskItem')
 
 describe(`TaskItem model`, () => {
+  let taskItem;
+  const createdAt = (Number(new Date()) - 1000 * 60 * 60 * 24 * 3)
   beforeEach(() => {
     return db.sync({ force: true })
   })
 
-  describe(`taskItem instance`, () => {
-    let taskItem;
-    const completed = (new Date() + 1000 * 60 * 60 * 24 * 4.25)
-    console.log(`completed >>>>`, completed)
+  describe(`incomplete taskItem`, () => {
     beforeEach(() => {
       return TaskItem.create({
         value: 10,
-        completed
+        createdAt
       })
         .then(item => {
-          console.log(item.dataValues)
           taskItem = item
         })
     })
 
-    it(`tests something`, () => {
-      expect(taskItem.value).to.be.equal(10)
+    it(`calculates days since last completed correctly`, () => {
+      expect(taskItem.days).to.be.equal(3)
     })
+    it(`current points are calculated correctly`, () => {
+      expect(taskItem.points).to.be.equal(30)
+    })
+
+  })
+  describe(`completed taskItem`, () => {
+    beforeEach(() => {
+      const completed = createdAt + 1000 * 60 * 60 * 24 * 5
+      taskItem.update({ completed })
+    })
+
+    it(`calculates days from created to completed correctly`, () => {
+      expect(taskItem.days).to.be.equal(5)
+    })
+    it(`total points are calculated correctly`, () => {
+      expect(taskItem.points).to.be.equal(50)
+    })
+
 
   }) // end describe('taskItem instance')
 }) // end describe('Task model')
