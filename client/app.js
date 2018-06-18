cd32b9d740819b2a720ab90636da39aaf3595365
chrisstpehenmiller <csmiller4567@gmail.com>
2018-06-18 15:28:10 -0500

    redesign

--- client/app.js
+++ client/app.js
@@ -11,9 +11,9 @@ const App = () => {
       <span>dwellplayd is a react native app designed to gamify and incentivize completing communal tasks in a shared living space</span>
       <a href="https://github.com/VCCD/dwellplayd" target="_blank" rel="noopener noreferrer">
         <div id="gifs">
-          <img src="/dwellplayd-gif-1.gif" />
-          <img src="/dwellplayd-gif-2.gif" />
           <img src="/dwellplayd-gif-3.gif" />
+          <img src="/dwellplayd-gif-2.gif" />
+          <img src="/dwellplayd-gif-1.gif" />
         </div>
       </a>
     </div>
